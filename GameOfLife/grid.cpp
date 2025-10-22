19feeec30109b1041087b884dbf1c2a52e005cbd
jdav892 <jadavila9@gmail.com>
2025-10-22 04:38:12 -0400

    feat: create grid lines

--- GameOfLife/grid.cpp
+++ GameOfLife/grid.cpp
@@ -8,7 +8,7 @@ void Grid::Draw()
     for(int column = 0; column < columns; column++)
     {
       Color color = cells[row][column] ? Color{0, 255, 0, 255} : Color{55, 55, 55, 255};
-      DrawRectangle(column * cellSize, row * cellSize, cellSize, cellSize, color);
+      DrawRectangle(column * cellSize, row * cellSize, cellSize - 1, cellSize - 1, color);
     }
   }
 }
